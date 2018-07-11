ffcb6f3bd65d90e52f6447d75ce3b58c7a006437
cypherpunks <cypherpunks@torproject.org>
2018-07-11 01:15:35 +0000

    net: use the public buf_t API

--- src/lib/net/buffers_net.c
+++ src/lib/net/buffers_net.c
@@ -9,7 +9,6 @@
  * \brief Read and write data on a buf_t object.
  **/
 
-#define BUFFERS_PRIVATE
 #include "lib/net/buffers_net.h"
 #include "lib/container/buffers.h"
 #include "lib/log/log.h"
@@ -31,17 +30,15 @@
 #endif /* defined(PARANOIA) */
 
 /** Read up to <b>at_most</b> bytes from the socket <b>fd</b> into
- * <b>chunk</b> (which must be on <b>buf</b>). If we get an EOF, set
+ * <b>buf</b> using temp buffer <b>data</b>. If we get an EOF, set
  * *<b>reached_eof</b> to 1.  Return -1 on error, 0 on eof or blocking,
  * and the number of bytes read otherwise. */
 static inline int
-read_to_chunk(buf_t *buf, chunk_t *chunk, tor_socket_t fd, size_t at_most,
-              int *reached_eof, int *socket_error)
+read_to_buf(buf_t *buf, char *data, tor_socket_t fd, size_t at_most,
+            int *reached_eof, int *socket_error)
 {
   ssize_t read_result;
-  if (at_most > CHUNK_REMAINING_CAPACITY(chunk))
-    at_most = CHUNK_REMAINING_CAPACITY(chunk);
-  read_result = tor_socket_recv(fd, CHUNK_WRITE_PTR(chunk), at_most, 0);
+  read_result = tor_socket_recv(fd, data, at_most, 0);
 
   if (read_result < 0) {
     int e = tor_socket_errno(fd);
@@ -59,10 +56,9 @@ read_to_chunk(buf_t *buf, chunk_t *chunk, tor_socket_t fd, size_t at_most,
     *reached_eof = 1;
     return 0;
   } else { /* actually got bytes. */
-    buf->datalen += read_result;
-    chunk->datalen += read_result;
+    buf_add(buf, data, read_result);
     log_debug(LD_NET,"Read %ld bytes. %d on inbuf.", (long)read_result,
-              (int)buf->datalen);
+              (int)buf_datalen(buf));
     tor_assert(read_result < INT_MAX);
     return (int)read_result;
   }
@@ -84,31 +80,23 @@ buf_read_from_socket(buf_t *buf, tor_socket_t s, size_t at_most,
    */
   int r = 0;
   size_t total_read = 0;
+  char tmp[256];
 
   check();
   tor_assert(reached_eof);
   tor_assert(SOCKET_OK(s));
 
-  if (BUG(buf->datalen >= INT_MAX))
+  if (BUG(buf_datalen(buf) >= INT_MAX))
     return -1;
-  if (BUG(buf->datalen >= INT_MAX - at_most))
+  if (BUG(buf_datalen(buf) >= INT_MAX - at_most))
     return -1;
 
   while (at_most > total_read) {
     size_t readlen = at_most - total_read;
-    chunk_t *chunk;
-    if (!buf->tail || CHUNK_REMAINING_CAPACITY(buf->tail) < MIN_READ_LEN) {
-      chunk = buf_add_chunk_with_capacity(buf, at_most, 1);
-      if (readlen > chunk->memlen)
-        readlen = chunk->memlen;
-    } else {
-      size_t cap = CHUNK_REMAINING_CAPACITY(buf->tail);
-      chunk = buf->tail;
-      if (cap < readlen)
-        readlen = cap;
-    }
+    if (readlen > sizeof tmp)
+      readlen = sizeof tmp;
 
-    r = read_to_chunk(buf, chunk, s, readlen, reached_eof, socket_error);
+    r = read_to_buf(buf, tmp, s, readlen, reached_eof, socket_error);
     check();
     if (r < 0)
       return r; /* Error */
@@ -121,20 +109,18 @@ buf_read_from_socket(buf_t *buf, tor_socket_t s, size_t at_most,
   return (int)total_read;
 }
 
-/** Helper for buf_flush_to_socket(): try to write <b>sz</b> bytes from chunk
- * <b>chunk</b> of buffer <b>buf</b> onto socket <b>s</b>.  On success, deduct
+/** Helper for buf_flush_to_socket(): try to write <b>sz</b> bytes from
+ * <b>data</b> of buffer <b>buf</b> onto socket <b>s</b>.  On success, deduct
  * the bytes written from *<b>buf_flushlen</b>.  Return the number of bytes
  * written on success, 0 on blocking, -1 on failure.
  */
 static inline int
-flush_chunk(tor_socket_t s, buf_t *buf, chunk_t *chunk, size_t sz,
-            size_t *buf_flushlen)
+flush_data(tor_socket_t s, buf_t *buf, const char *data, size_t sz,
+           size_t *buf_flushlen)
 {
   ssize_t write_result;
 
-  if (sz > chunk->datalen)
-    sz = chunk->datalen;
-  write_result = tor_socket_send(s, chunk->data, sz, 0);
+  write_result = tor_socket_send(s, data, sz, 0);
 
   if (write_result < 0) {
     int e = tor_socket_errno(s);
@@ -170,10 +156,12 @@ buf_flush_to_socket(buf_t *buf, tor_socket_t s, size_t sz,
    */
   int r;
   size_t flushed = 0;
+  char tmp[256];
+
   tor_assert(buf_flushlen);
   tor_assert(SOCKET_OK(s));
-  if (BUG(*buf_flushlen > buf->datalen)) {
-    *buf_flushlen = buf->datalen;
+  if (BUG(*buf_flushlen > buf_datalen(buf))) {
+    *buf_flushlen = buf_datalen(buf);
   }
   if (BUG(sz > *buf_flushlen)) {
     sz = *buf_flushlen;
@@ -182,13 +170,13 @@ buf_flush_to_socket(buf_t *buf, tor_socket_t s, size_t sz,
   check();
   while (sz) {
     size_t flushlen0;
-    tor_assert(buf->head);
-    if (buf->head->datalen >= sz)
+    if (sz <= sizeof tmp)
       flushlen0 = sz;
     else
-      flushlen0 = buf->head->datalen;
+      flushlen0 = sizeof tmp;
 
-    r = flush_chunk(s, buf, buf->head, flushlen0, buf_flushlen);
+    buf_peek(buf, tmp, flushlen0);
+    r = flush_data(s, buf, tmp, flushlen0, buf_flushlen);
     check();
     if (r < 0)
       return r;
