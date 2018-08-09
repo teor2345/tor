ccead595815d6b1b396f047ebca5d58602b1fd51
teor <teor@torproject.org>
2018-08-09 11:59:55 +1000

    Rust: Apply hello71/misc-rust to src/test/test_rust.sh
    test_rust.sh uses the global cargo cache instead of .cargo in the
    build directory.
    
    Part of 26038.

--- src/test/test_rust.sh
+++ src/test/test_rust.sh
@@ -5,11 +5,16 @@ set -e
 
 export LSAN_OPTIONS=suppressions=${abs_top_srcdir}/src/test/rust_supp.txt
 
+# check that CARGO_HOME is not relative (it will be interpreted
+# relative to $abs_top_builddir/src/rust instead of $abs_top_builddir)
+if [ -n "$CARGO_HOME" ] && [ "${CARGO_HOME:1:1}" != / ]; then
+    echo "Tor's build system only supports absolute path (or unset) CARGO_HOME."
+fi
+
 for cargo_toml_dir in "${abs_top_srcdir:-../../..}"/src/rust/*; do
     if [ -e "${cargo_toml_dir}/Cargo.toml" ]; then
 	cd "${cargo_toml_dir}" && \
 	    CARGO_TARGET_DIR="${abs_top_builddir:-../../..}/src/rust/target" \
-	    CARGO_HOME="${abs_top_builddir:-../../..}/src/rust/.cargo" \
 	    "${CARGO:-cargo}" test ${CARGO_ONLINE-"--frozen"} \
 	    ${EXTRA_CARGO_OPTIONS} \
 	    --manifest-path "${cargo_toml_dir}/Cargo.toml" || exitcode=1
