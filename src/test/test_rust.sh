84f30216be8be30e673c225b0dd65bb2f7b21db9
teor <teor@torproject.org>
2018-08-09 12:07:43 +1000

    Rust: cd ${abs_top_builddir}/src/rust in test_rust.sh
    For consistency with src/rust/tor_rust/include.am.
    
    And fix some confusing formatting in test_rust.sh
    
    Part of 26497.

--- src/test/test_rust.sh
+++ src/test/test_rust.sh
@@ -3,7 +3,7 @@
 
 set -e
 
-export LSAN_OPTIONS=suppressions=${abs_top_srcdir}/src/test/rust_supp.txt
+export LSAN_OPTIONS=suppressions=${abs_top_srcdir:-../../..}/src/test/rust_supp.txt
 
 # check that CARGO_HOME is not relative (it will be interpreted
 # relative to $abs_top_builddir/src/rust instead of $abs_top_builddir)
@@ -13,11 +13,11 @@ fi
 
 for cargo_toml_dir in "${abs_top_srcdir:-../../..}"/src/rust/*; do
     if [ -e "${cargo_toml_dir}/Cargo.toml" ]; then
-	cd "${cargo_toml_dir}" && \
+	    cd "${abs_top_builddir:-../../..}/src/rust" && \
 	    CARGO_TARGET_DIR="${abs_top_builddir:-../../..}/src/rust/target" \
-	    "${CARGO:-cargo}" test ${CARGO_ONLINE-"--frozen"} \
-	    ${EXTRA_CARGO_OPTIONS} \
-	    --manifest-path "${cargo_toml_dir}/Cargo.toml" || exitcode=1
+	        "${CARGO:-cargo}" test ${CARGO_ONLINE-"--frozen"} \
+	        ${EXTRA_CARGO_OPTIONS} \
+	        --manifest-path "${cargo_toml_dir}/Cargo.toml" || exitcode=1
     fi
 done
 
