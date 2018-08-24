7bf7034965ee8be7f9926d372a11082fde302b8d
teor <teor@torproject.org>
2018-08-24 10:59:02 +1000

    Deliberately fail make check-spaces

--- src/app/config/config.h
+++ src/app/config/config.h
@@ -1,3 +1,8 @@
+
+
+/* Fail make check-spaces */
+
+
 /* Copyright (c) 2001 Matej Pfajfar.
  * Copyright (c) 2001-2004, Roger Dingledine.
  * Copyright (c) 2004-2006, Roger Dingledine, Nick Mathewson.
